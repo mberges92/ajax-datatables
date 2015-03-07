a10731dad44d2357c01265c3693e1d222649e368
mberges92 <mberges92@gmail.com>
2015-03-07 23:50:47 +0100

    Ejercicio completo funcionando

---
+++ app/php/mysql.php
@@ -0,0 +1,13 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+/*
+$gaSql['user'] = "root";
+$gaSql['password'] = "root";
+$gaSql['db'] = "datatables";
+$gaSql['server'] = "localhost";
+*/
+$gaSql['user'] = "maberges_dwec";
+$gaSql['password'] = "123456";
+$gaSql['db'] = "maberges_datatables";
+$gaSql['server'] = "localhost";
+?>
